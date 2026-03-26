077ba13818b756e41c58c7d6eaa7ed5ddd623f29
hoangkha442 <125436413+hoangkha442@users.noreply.github.com>
2026-03-26 20:18:16 +0700

    update hosting

--- server/index.js
+++ server/index.js
@@ -1,32 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Middlewares
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+// middlewares
+const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
-// APIs
+// apis
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello from server!' });
 });
 app.use('/api/admin', require('./api/admin.js'));
 app.use('/api/customer', require('./api/customer.js'));
 
-
+// deployment
+const path = require('path');
 app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
-app.get('/admin/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
 });
 app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
-});
-
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
 });
